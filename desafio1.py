6b2478752bb6a96da023bdeafbbac9112f150e71
Kelvinmp <kelvinmoya01@gmail.com>
2026-05-27 17:31:10 -0400

    Solucion desafio 1

--- desafio1.py
+++ desafio1.py
@@ -1,10 +1,38 @@
-palabras = ["gusta", "Me", "helado", "comer", "mucho"]
-indices = [2, 1, 5, 4, 3]
-resultado = [None] * len(palabras)   
+import sys
 
-for i in range(len(palabras)):
-    pos = indices[i] - 1          
-    resultado[pos] = palabras[i]
+def ordenar_palabras(palabras,indices):
+    resultado = [None] * len(palabras)   
 
-oracion = ' '.join(resultado)
-print(oracion)
+    for i in range(len(palabras)):
+        pos = indices[i] - 1          
+        resultado[pos] = palabras[i]
+    oracion = ' '.join(resultado)
+
+    return oracion
+
+nombre_archivo = sys.argv[1]
+
+archivo = open(nombre_archivo, "r")
+
+for linea in archivo:
+    linea = linea.strip()
+    if linea == "":
+        continue
+    
+    partes = linea.split(";")
+   
+    palabras_str = partes[0].strip()
+    palabras = palabras_str.split()
+   
+    indices_str = partes[1].strip()
+    indices_texto = indices_str.split()
+    
+    indices = []
+    for num in indices_texto:
+        indices.append(int(num))
+   
+    oracion_final = ordenar_palabras(palabras, indices)
+    print(oracion_final)
+
+
+archivo.close()
