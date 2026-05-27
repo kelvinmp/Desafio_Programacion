6ae2a134c62f8482408a01b1bae9c7c48a0d86a0
Kelvinmp <kelvinmoya01@gmail.com>
2026-05-27 16:51:21 -0400

    logica para ordenar las palabras

--- desafio1.py
+++ desafio1.py
@@ -1,2 +1,10 @@
+palabras = ["gusta", "Me", "helado", "comer", "mucho"]
+indices = [2, 1, 5, 4, 3]
+resultado = [None] * len(palabras)   
 
+for i in range(len(palabras)):
+    pos = indices[i] - 1          
+    resultado[pos] = palabras[i]
 
+oracion = ' '.join(resultado)
+print(oracion)
